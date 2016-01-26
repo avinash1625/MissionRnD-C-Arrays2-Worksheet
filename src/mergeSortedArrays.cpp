d780d64a968b3524250beb884c16420fa768e8be
avinash1625 <avinash1625@missionrnd.com>
2016-01-26 11:03:24 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -14,14 +14,80 @@ NOTES:
 */
 
 #include <iostream>
+#include<stdlib.h>
 
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
-
-struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	if (A==NULL||B==NULL)
-		return NULL;
+int _int(char *date, int i, int j)
+{
+	int result = 0;
+	for (; i<j; i++)
+	{
+		result = result * 10 + (date[i] - '0');
+	}
+	return result;
 }
+struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
+		if (A == NULL || B == NULL)
+			return NULL;
+		int i = 0, j = 0,k=0, day = 0,flag=0, month = 0, year = 0, day1 = 0, month1 = 0, year1 = 0;
+		struct transaction *result = (struct transaction*)malloc((ALen + BLen)*sizeof(struct transaction));
+		while (i < ALen&&j < BLen)
+		{
+			day = _int(A[i].date, 0, 2);
+			month = _int(A[i].date, 3, 5);
+			year = _int(A[i].date, 6, 10);
+			day1 = _int(B[j].date, 0, 2);
+			month1 = _int(B[j].date, 3, 5);
+			year1 = _int(B[j].date, 6,10 );
+
+			if (year1 < year)
+			{
+				result[k] = B[j];
+				k++;
+				j++;
+				flag = 1;
+			}
+			else if (year1 == year&&month1 < month)
+			{
+				result[k] = B[j];
+				k++;
+				j++;
+				flag= 1;
+			}
+			else  if (year1 == year&&month1 == month&&day1 < day)
+			{
+				result[k] = B[j];
+				k++;
+				j++;
+				flag = 1;
+			}
+			if (flag != 1)
+			{
+				result[k] = A[i];
+				i++;
+				k++;
+			}
+			day1 = 0;
+			month1 = 0;
+			year1 = 0;
+			flag= 0;
+		}
+		while (i < ALen)
+		{
+			result[k] = A[i];
+			i++;
+			k++;
+		}
+		while (j < BLen)
+		{
+			result[k] = B[j];
+			j++;
+			k++;
+		}
+		return result;
+}
+	
